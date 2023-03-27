db1a89d3c3e230d9898d3ac474fc1ff6fd717cbe
Agafonov_German\prog <agafonovgerman@yandex.ru>
2023-03-27 16:22:54 +0300

    Для теста "Проверка поиска по определению победителя" добавлен шаг "scroll_element_on_pixels" и изменен шаг "steps.click_button(getCheckboxInFilter(4))" в связи с изменениями в новом дизайне

--- src/test/java/testpackage/WhenTabTendersTest.java
+++ src/test/java/testpackage/WhenTabTendersTest.java
@@ -347,9 +347,10 @@ public class WhenTabTendersTest extends TabTenders {
     public void checkSearchByExecutionContract(){
 
         steps.click_button(tabListAutoSearch);
+        steps.scroll_element_on_pixels("250", listAutoSearchToScroll);
         steps.click_button(buttonCheckSearchByMineTenders);
         steps.click_button(filterSearchByMineTendersOrContractsStatus);
-        steps.click_button(getCheckboxInFilter(5));
+        steps.click_button(getCheckboxInFilter(4));
         steps.click_button(buttonSearch);
         steps.wait_a_bit(3000);
         steps.check_search_by_execution_contract();
