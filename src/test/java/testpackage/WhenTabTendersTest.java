c0643c9eeb08f776504942060694be0d9d2626fc
Agafonov_German\prog <agafonovgerman@yandex.ru>
2023-03-27 16:23:58 +0300

    Для теста "Проверка поиска по архиву" добавлен шаг "scroll_element_on_pixels" и изменен шаг "steps.click_button(getCheckboxInFilter(5))" в связи с изменениями в новом дизайне

--- src/test/java/testpackage/WhenTabTendersTest.java
+++ src/test/java/testpackage/WhenTabTendersTest.java
@@ -361,9 +361,10 @@ public class WhenTabTendersTest extends TabTenders {
     public void checkSearchByArchiveTenders(){
 
         steps.click_button(tabListAutoSearch);
+        steps.scroll_element_on_pixels("250", listAutoSearchToScroll);
         steps.click_button(buttonCheckSearchByMineTenders);
         steps.click_button(filterSearchByMineTendersOrContractsStatus);
-        steps.click_button(getCheckboxInFilter(6));
+        steps.click_button(getCheckboxInFilter(5));
         steps.click_button(buttonSearch);
         steps.wait_a_bit(3000);
         steps.check_search_by_archive_tenders();
